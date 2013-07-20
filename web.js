b085c762f7133afbe23ac125477ce95a988017e0
Matt Simms <mcsimms@gmail.com>
2013-07-20 05:04:45 +0000

    Read index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs'); 
 var app = express();
 app.use(express.logger());
 
+var f = fs.readFileSync('index.html', 'utf-8');
+var fb = new Buffer(f, 'utf-8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fb);
 });
 
 var port = process.env.PORT || 5000;
